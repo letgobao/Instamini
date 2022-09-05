b6cdb6d18247b312722442497ce2925cd40dfe4d
dutran917 <ductcp@gmail.com>
2022-09-05 19:13:13 +0700

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 /** @format */
 
-import Navigation from "./components/navbar/Navigation";
 import "./App.css";
 import Auth from "./pages/auth/Auth";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Register from "./pages/auth/Register";
 function App() {
   return (
-    <div className='App'>
-      <Navigation />
-      Instamini
-      <Auth />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Auth />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
