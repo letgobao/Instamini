c4617a2c711b2c520d62268f624e383386159faf
bao <mailforbao@gmail.com>
2022-09-05 19:14:46 +0700

    save

--- client/src/components/navbar/Navigation.jsx
+++ client/src/components/navbar/Navigation.jsx
@@ -1,17 +1,52 @@
 /** @format */
 
 import React from "react";
+import {
+  SearchIcon,
+  HomeIcon,
+  HomeFillIcon,
+  ChatFillIcon,
+  ChatIcon,
+  PlusFillIcon,
+  PlusIcon,
+  NotificationFillIcon,
+  NotificationIcon,
+  AvatarIcon,
+} from "../../imgs/icons/index.js";
+
+import './Navigation.css'
 const Navigation = () => {
   return (
     <nav className='navigation'>
       <div className='nav-box'>
-        <div className='logo-nav'>
-            <div className="logo">
-              Instamini
+        <div className="nav-container">
+          <div className='nav-logo'>
+            <div className='logo'>Instamini</div>
+          </div>
+          <div className='nav-search'>
+            <input type='text' placeholder='Tìm kiếm' />
+            <div className='search-box'>
+              <img src={SearchIcon} alt='Tìm kiếm' />
+              <span className='search-title'>Tìm kiếm</span>
             </div>
-        </div>
-        <div className='search-box'>
-
+          </div>
+          <div className='nav-options'>
+            <div className="option">
+              <img src={true ? HomeIcon : HomeFillIcon} alt="Home" />
+            </div>
+            <div className="option">
+              <img src={true ? ChatIcon : ChatFillIcon} alt="Nhắn tin" />
+            </div>
+            <div className="option">
+              <img src={true ? PlusIcon : PlusFillIcon} alt="Đăng bài" />
+            </div>
+            <div className="option">
+              <img src={true ? NotificationIcon : NotificationFillIcon} alt="Thông báo" />
+            </div>
+            <div className="option">
+              <img src={AvatarIcon} alt="Cá nhân" />
+            </div>
+          </div>
         </div>
       </div>
     </nav>
