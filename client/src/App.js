86461147688b905d884417a6ad3d0d5b8e45629f
dutran917 <ductcp@gmail.com>
2022-09-05 17:48:34 +0700

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Auth from "./pages/auth/Auth";
 
 function App() {
-  return <div className="App">Instamini</div>;
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
 }
 
 export default App;
