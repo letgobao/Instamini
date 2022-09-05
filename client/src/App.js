b1b6fef04bc9cd9adb9629f4d94886a07498dee4
bao <mailforbao@gmail.com>
2022-09-05 17:48:27 +0700

    add font

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+/** @format */
+
+import Navigation from "./components/navbar/Navigation";
 import "./App.css";
 
 function App() {
-  return <div className="App">Instamini</div>;
+  return (
+    <div className='App'>
+      <Navigation />
+      Instamini
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/navbar/Navigation.jsx
+++ client/src/components/navbar/Navigation.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+/** @format */
 
+import React from "react";
 const Navigation = () => {
   return (
-    <div>
-        n
-    </div>
-  )
-}
+    <nav className='navigation'>
+      <div className='nav-box'>
+        <div className='logo-nav'>
+            <div className="logo">
+              Instamini
+            </div>
+        </div>
+        <div className='search-box'>
 
-export default Navigation
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
