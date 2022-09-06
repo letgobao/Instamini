dcbbec9b34dba061ade403d41ff619c7f781791c
dutran917 <ductcp@gmail.com>
2022-09-06 17:28:51 +0700

    ps

---
+++ client/src/data/personal.js
@@ -0,0 +1,11 @@
+export const ronaldo = {
+  username: "cristiano",
+  avatarURL:
+    "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg",
+  isFollowing: false,
+  post: 15,
+  follower: 478,
+  following: 100,
+  fullName: "Cristiano Ronaldo",
+  description: "MU vo dich",
+};
--- client/src/pages/personal/Personal.jsx
+++ client/src/pages/personal/Personal.jsx
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
-const Personal = () => {
+import { Typography } from "@mui/material";
+const Personal = ({ data }) => {
+  const username = "du.tran917";
+  const [isFollowing, setIsFollowing] = useState(data.isFollowing);
   return (
     <div className="personal">
       <div className="personal-header">
         <div className="personal-avatar">
           <Avatar
-            src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg"
+            src={data.avatarURL}
             alt="person"
             sx={{
               width: "40px",
               height: "40px",
             }}
           />
+        </div>
+        <div className="personal-info">
+          <div className="p-name">
+            <Typography variant="h3">{data.username}</Typography>
+            {username === data.username ? (
+              <div className="p-edit">Chỉnh sửa trang cá nhân</div>
+            ) : (
+              <div className="p-connect">
+                <div
+                  onClick={() => {
+                    setIsFollowing(!isFollowing);
+                  }}
+                >
+                  {isFollowing ? (
+                    <span>Bỏ theo dõi</span>
+                  ) : (
+                    <span>Theo dõi</span>
+                  )}
+                </div>
+                <div>Nhắn tin</div>
+              </div>
+            )}
+          </div>
+          <div className="p-number">
+            <div>{`${data.post} bài viết`}</div>
+            <div>{`${data.follower} người theo dõi`}</div>
+            <div>{`Đang theo dõi ${data.following} người dùng`}</div>
+          </div>
+          <div className="description">
+            <div>{data.fullName}</div>
+            <div>{data.description}</div>
+          </div>
         </div>
       </div>
     </div>
