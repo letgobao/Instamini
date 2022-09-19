2073a8f1ba83b814247caf809c4fc951a79ec55c
dutran917 <ductcp@gmail.com>
2022-09-19 20:32:14 +0700

    ps

--- client/src/data/personal.js
+++ client/src/data/personal.js
@@ -1,5 +1,5 @@
 export const ronaldo = {
-  username: "cristiano",
+  username: "ronaldo",
   avatarURL:
     "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg",
   isFollowing: false,
@@ -7,5 +7,17 @@ export const ronaldo = {
   follower: 478,
   following: 100,
   fullName: "Cristiano Ronaldo",
-  description: "MU vo dich",
+  description:
+    "Proplayer hiện đang thi đấu ở vị trí tiền đạo cho câu lạc bộ Premier League Manchester United và là đội trưởng của đội tuyển bóng đá quốc gia Bồ Đào Nha.",
+};
+export const dutran = {
+  username: "du.tran917",
+  avatarURL:
+    "https://instagram.fhan14-1.fna.fbcdn.net/v/t51.2885-19/306675117_817638209590877_3197990813400422886_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fhan14-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=JLpte59pEREAX-GsYdq&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT_nAAahXSJTu3n-iPQYqJPAxUrmX30GLi44PpFwVhqzLg&oe=632E5D09&_nc_sid=8fd12b",
+  isFollowing: false,
+  post: 1,
+  follower: 333,
+  following: 13,
+  fullName: "Du Tran",
+  description: "Dep trai hoc gioi",
 };
--- client/src/pages/personal/Personal.jsx
+++ client/src/pages/personal/Personal.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import "./Personal.css";
+import GridOnIcon from "@mui/icons-material/GridOn";
+import OndemandVideoOutlinedIcon from "@mui/icons-material/OndemandVideoOutlined";
+import AssignmentIndOutlinedIcon from "@mui/icons-material/AssignmentIndOutlined";
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 const Personal = ({ data }) => {
   const username = "du.tran917";
   const [isFollowing, setIsFollowing] = useState(data.isFollowing);
+  const [selectedOption, setSelectedOption] = useState(1);
+  const url = useLocation();
+  useEffect(() => {
+    const path = url.pathname.split("/")[2];
+    if (path === "reels") {
+      setSelectedOption(2);
+    } else if (path === "tags") {
+      setSelectedOption(3);
+    } else {
+      setSelectedOption(1);
+    }
+  });
   return (
     <div className="personal">
       <div className="personal-header">
@@ -12,14 +30,14 @@ const Personal = ({ data }) => {
             src={data.avatarURL}
             alt="person"
             sx={{
-              width: "40px",
-              height: "40px",
+              width: "150px",
+              height: "150px",
             }}
           />
         </div>
         <div className="personal-info">
           <div className="p-name">
-            <Typography variant="h3">{data.username}</Typography>
+            <Typography variant="h4">{data.username}</Typography>
             {username === data.username ? (
               <div className="p-edit">Chỉnh sửa trang cá nhân</div>
             ) : (
@@ -29,25 +47,91 @@ const Personal = ({ data }) => {
                     setIsFollowing(!isFollowing);
                   }}
                 >
+                  <Button variant="outlined">Nhắn tin</Button>
                   {isFollowing ? (
-                    <span>Bỏ theo dõi</span>
+                    <Button variant="outlined">Bỏ theo dõi</Button>
                   ) : (
-                    <span>Theo dõi</span>
+                    <Button variant="contained">Theo dõi</Button>
                   )}
                 </div>
-                <div>Nhắn tin</div>
               </div>
             )}
           </div>
-          <div className="p-number">
-            <div>{`${data.post} bài viết`}</div>
-            <div>{`${data.follower} người theo dõi`}</div>
-            <div>{`Đang theo dõi ${data.following} người dùng`}</div>
-          </div>
+
+          <ul className="p-number">
+            <li>
+              <span>{data.post}</span> bài viết
+            </li>
+            <li>
+              <span>{data.follower}</span> người theo dõi
+            </li>
+            <li>
+              Đang theo dõi <span>{data.following}</span> người dùng
+            </li>
+          </ul>
+
           <div className="description">
-            <div>{data.fullName}</div>
-            <div>{data.description}</div>
+            <div className="des-fullname">{data.fullName}</div>
+            <div className="des-content">{data.description}</div>
           </div>
+        </div>
+      </div>
+      <div className="personal-post">
+        <div className="p-post-option">
+          <Link
+            className={selectedOption === 1 ? "p-option-select" : ""}
+            to={data.username}
+            onClick={(e) => {
+              setSelectedOption(1);
+            }}
+          >
+            <div>
+              <GridOnIcon
+                sx={{
+                  width: "15px",
+                  height: "15px",
+                  marginRight: "5px",
+                }}
+              />
+              <span>Bài đăng</span>
+            </div>
+          </Link>
+          <Link
+            className={selectedOption === 2 ? "p-option-select" : ""}
+            to="reels"
+            onClick={() => {
+              setSelectedOption(2);
+            }}
+          >
+            <div>
+              <OndemandVideoOutlinedIcon
+                sx={{
+                  width: "15px",
+                  height: "15px",
+                  marginRight: "5px",
+                }}
+              />
+              <span>Reels</span>
+            </div>
+          </Link>
+          <Link
+            className={selectedOption === 3 ? "p-option-select" : ""}
+            to="tags"
+            onClick={() => {
+              setSelectedOption(3);
+            }}
+          >
+            <div>
+              <AssignmentIndOutlinedIcon
+                sx={{
+                  width: "15px",
+                  height: "15px",
+                  marginRight: "5px",
+                }}
+              />
+              <span>Được gắn thẻ</span>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
